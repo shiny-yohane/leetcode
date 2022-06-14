69cfe67e7ac6b5215b38b33b9c6ce024388e6ea5
Yohane <43869960+shiny-yohane@users.noreply.github.com>
2022-06-14 12:07:04 +0900

    Merge-Two-Sorted-Lists v2

--- 21-Merge-Two-Sorted-Lists.py
+++ 21-Merge-Two-Sorted-Lists.py
@@ -4,38 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if not list1:
-            return list2
-        
-        if not list2:
-            return list1
-        
-        if not list1 and not list2:
-            return None
-        
-        new_head = ListNode()
-        new_curr = new_head
-        
-        list1_curr = list1
-        list2_curr = list2
-        
-        while list1_curr and list2_curr:
-            if list1_curr.val < list2_curr.val:
-                new_curr.next = ListNode(list1_curr.val)
-                new_curr      = new_curr.next
-                list1_curr    = list1_curr.next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]):
+    
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
             else:
-                new_curr.next = ListNode(list2_curr.val)
-                new_curr      = new_curr.next
-                list2_curr    = list2_curr.next
-            
-        
-        if list1_curr:
-            new_curr.next = list1_curr
-            
-        if list2_curr:
-            new_curr.next = list2_curr
-        
-        return new_head.next
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1:
+            curr.next = list1
+
+        if list2:
+            curr.next = list2
+
+        return dummy.next
