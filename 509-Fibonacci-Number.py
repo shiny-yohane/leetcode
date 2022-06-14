bc9dd467ce1e20aa51c57c3087b0672e48149cf3
Yohane <43869960+shiny-yohane@users.noreply.github.com>
2022-06-14 23:34:27 +0900

    fib v2

--- 509-Fibonacci-Number.py
+++ 509-Fibonacci-Number.py
@@ -1,6 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return n
-        else:
-            return self.fib(n - 1) + self.fib(n - 2)
+        memo = {0: 0, 1: 1}
+        
+        for i in range(2, n + 1):
+            memo[i] = memo[i - 1] + memo [i - 2]
+        
+        return memo[n]
