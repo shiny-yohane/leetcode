be72ad7e9eb41b7af41c471a035e29f0a492cc7d
Yohane <43869960+shiny-yohane@users.noreply.github.com>
2022-06-24 17:11:41 +0900

    Subsets v2

--- 78-Subsets.py
+++ 78-Subsets.py
@@ -1,19 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
+        subset = []
         
-        def dfs(i, curr):
-            if (i >= len(nums)):
-                res.append(curr.copy())
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
                 return
             
-            n = nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
             
-            curr.append(n)
-            dfs(i + 1, curr)
+            subset.pop()
+            dfs(i + 1)
             
-            curr.pop()
-            dfs(i + 1, curr)
+        dfs(0)
             
-        dfs(0, [])
-        return res    
+        return res
